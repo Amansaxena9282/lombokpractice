fa659eb43971a751f24340c48b59894227a1a98b
Aman <saxenaaman682@gmail.com>
2023-01-28 20:22:27 +0530

    practice program

--- src/main/java/com/practice/lombokpractice/MyPractice.java
+++ src/main/java/com/practice/lombokpractice/MyPractice.java
@@ -5,6 +5,10 @@ import org.springframework.boot.SpringApplication;
 
 public class MyPractice {
     public static void main(String[] args) {
+//        SpringApplication.run(LombokpracticeApplication.class, args);
+//        User user=new User();
+//        user.setName("aman");
+//        System.out.println("eleserv softech="+user.getName());
         SpringApplication.run(LombokpracticeApplication.class, args);
         User user=new User();
         user.setName("aman");
