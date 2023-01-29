c29a3453cc8dca4547140c71ed567d5d8379bfac
Aman <saxenaaman682@gmail.com>
2023-01-29 17:51:43 +0530

    commiteed

--- src/main/java/com/practice/lombokpractice/Controller/TestController.java
+++ src/main/java/com/practice/lombokpractice/Controller/TestController.java
@@ -1,11 +1,25 @@
 package com.practice.lombokpractice.Controller;
 
+import com.practice.lombokpractice.model.User;
 import lombok.Data;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-@Data
+
 @RestController
+@RequestMapping("/api/v1")
 public class TestController
 {
+//    @PostMapping("/add")
+//    public User addUser@RequestMapping
+
+@GetMapping("/aman")
+    public String xyz(){
+    return "main pagal hu ";
+}
+
 
 }
+
