8f484dd70054b66f100f1565f593e3aed65098f6
Aman <saxenaaman682@gmail.com>
2023-01-29 18:48:21 +0530

    code commit

--- src/main/java/com/practice/lombokpractice/Controller/TestController.java
+++ src/main/java/com/practice/lombokpractice/Controller/TestController.java
@@ -3,17 +3,35 @@ package com.practice.lombokpractice.Controller;
 import com.practice.lombokpractice.model.User;
 import lombok.Data;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1")
-public class TestController
-{
-//    @PostMapping("/add")
-//    public User addUser@RequestMapping
+public class TestController {
+    @PostMapping("/add")
+    public User addUser(@RequestBody User user) {
+        return user;
+    }
+
+    @PutMapping("/update")
+    public User update(@RequestBody User user) {
+        return user;
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String deletedata(@PathVariable int id) {
+        return "your id" + "deleted success" + id;
+    }
+
+    @GetMapping("/ex2/{age}/data")
+    public User getuserdetails(@PathVariable int age, @RequestParam String name) {
+        String name1 = name ;
+        int age1 = age;
+        User user = new User();
+        return user;
+    }
+
+
 
 @GetMapping("/aman")
     public String xyz(){
